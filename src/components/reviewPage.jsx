3911c537ead2911af3085f3c4af7a8c214cac713
Shrey-raj-singh <rjshrey23@gmail.com>
2024-07-11 21:07:05 +0530

    --

--- src/components/reviewPage.jsx
+++ src/components/reviewPage.jsx
@@ -2,7 +2,7 @@ const ReviewPage = () => {
     return <main className="w-full h-full">
         <div className="left-arrow"></div>
         <div className="content w-full h-full">
-
+        <h1 className="text-gray-400">Will make it later</h1>
         </div>
     </main>
 }
